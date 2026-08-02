4d36805f363ed97a06fd4bd7655b2ceea5a6796b
matheus-lopesm <matheuslmachado03@gmail.com>
2026-08-02 11:55:13 -0300

    fix: remove duplicated

--- tests/tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(metan)
-
-test_check("metan")
--- tests/tests/testthat/helper-round.R
+++
@@ -1,3 +0,0 @@
-round_num <- function(x, digits = 5) {
-  dplyr::mutate(x, dplyr::across(dplyr::where(is.numeric), \(v) round(v, digits)))
-}
